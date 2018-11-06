b751cba807aff1d22b88324c93d8445ed9107da4
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-06 12:22:09 +0100

    1.9

--- index.js
+++ index.js
@@ -3,52 +3,60 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
+    /////////////////
+
     var Connection = require('tedious').Connection;
-     var config = {
-         userName: 'MSM_Promotion_Admin',
-         password: 'Seebaer181050',
-         server: 'msm.database.windows.net',
-         // If you are on Microsoft Azure, you need this:
-         options: {encrypt: true, database: 'AdventureWorks'}
-     };
-     var connection = new Connection(config);
-     connection.on('connect', function(err) {
-     // If no error, then good to proceed.
-         console.log("Connected");
-         executeStatement();
-     });
-     var Request = require('tedious').Request;
-    var TYPES = require('tedious').TYPES;
-
-    function executeStatement() {
-        request = new Request("SELECT * FROM LeistungsKundenAccount", function(err) {
-        if (err) {
-            console.log(err);}
-        });
-        var result = "";
-        request.on('row', function(columns) {
-            columns.forEach(function(column) {
-              if (column.value === null) {
-                console.log('NULL');
-              } else {
-                result+= column.value + " ";
-              }
+         var config = {
+             userName: 'MSM_Promotion_Admin',
+             password: 'Seebaer181050',
+             server: 'msm.database.windows.net',
+             // If you are on Microsoft Azure, you need this:
+             options: {encrypt: true, database: 'AdventureWorks'}
+         };
+         var connection = new Connection(config);
+            connection.on('connect', function(err) {
+                // If no error, then good to proceed.
+                console.log("Connected");
+                executeStatement();
             });
-            console.log(result);
-            response.send(result);
-            result ="";
-        });
 
-        request.on('done', function(rowCount, more) {
-        console.log(rowCount + ' rows returned');
+        function executeStatement() {
+            request = new Request("SELECT * FROM LeistungsKundenAccount", function(err) {
+            if (err) {
+                console.log(err);}
+            });
+            var result = "";
+            request.on('row', function(columns) {
+                columns.forEach(function(column) {
+                  if (column.value === null) {
+                    console.log('NULL');
+                  } else {
+                    result+= column.value + " ";
+                  }
+                });
+                console.log(result);
+                response.send(result);
+                result ="";
+            });
 
-        });
-        connection.execSql(request);
-    }
+            request.on('done', function(rowCount, more) {
+            console.log(rowCount + ' rows returned');
+
+            });
+            connection.execSql(request);
+        }
+
+    });
+
+    var port = process.env.PORT || 1337;
+    server.listen(port);
+
+    console.log("Server running at http://localhost:%d", port);
 
 });
 
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log(
+var server = http.c
