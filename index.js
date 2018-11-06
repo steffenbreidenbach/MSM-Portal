8579e83d2b14298ed2806bbc9533a8e82cf60f0b
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-06 12:10:54 +0100

    1.6

--- index.js
+++ index.js
@@ -4,6 +4,48 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+    var Connection = require('tedious').Connection;
+     var config = {
+         userName: 'MSM_Promotion_Admin',
+         password: 'Seebaer181050',
+         server: 'msm.database.windows.net',
+         // If you are on Microsoft Azure, you need this:
+         options: {encrypt: true, database: 'AdventureWorks'}
+     };
+     var connection = new Connection(config);
+     connection.on('connect', function(err) {
+     // If no error, then good to proceed.
+         console.log("Connected");
+         executeStatement();
+     });
+     var Request = require('tedious').Request;
+    var TYPES = require('tedious').TYPES;
+
+    function executeStatement() {
+        request = new Request("SELECT * FROM LeistungsKundenAccount", function(err) {
+        if (err) {
+            console.log(err);}
+        });
+        var result = "";
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+              if (column.value === null) {
+                console.log('NULL');
+              } else {
+                result+= column.value + " ";
+              }
+            });
+            console.log(result);  
+            response.end(result);
+            result ="";
+        });
+
+        request.on('done', function(rowCount, more) {
+        console.log(rowCount + ' rows returned');
+
+        });
+        connection.execSql(request);
+    }
 
 });
 
