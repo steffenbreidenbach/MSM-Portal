731d175277090c24e0c9e5876d0e3dd4dbb8aeb7
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-06 12:34:01 +0100

    2.1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     /////////////////
-
+/*
     var Connection = require('tedious').Connection;
          var config = {
              userName: 'MSM_Promotion_Admin',
@@ -43,7 +43,7 @@ var server = http.createServer(function(request, response) {
 
             });
             connection.execSql(request);
-        }
+        } */
         response.end("test");
     });
 
