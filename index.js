44f8abe49e49d56064234637ae7fef92b2ba4db4
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-13 11:08:58 +0100

    3.7

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
@@ -11,3 +11,14 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+*/
+var express = require('express');
+
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+var port = process.env.PORT || 1337;
+app.listen(1337, function () {
+  console.log('Example app listening on port 3000!');
+});
