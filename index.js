5165120a65baf3f1622b9287faf20c68976cb53b
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-13 12:11:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 var Request = require('tedious').Request;
 
 var server = http.createServer(function(request, response) {
@@ -14,3 +14,108 @@ server.listen(port);
 console.log("Server running at http://localhost:%d", port);
 
 
+*/
+var express = require("express");
+var app = express();
+var Request = require('tedious').Request;
+
+
+app.set("view engine","ejs");
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/public/html/index.html");
+});
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/public/html/404.html");
+});
+
+app.get("/login",function(req,res){
+    getDataFromSQL();
+    res.sendFile(__dirname + "/public/html/login.html");
+});
+
+app.get("/login/:name",function(req,res){
+    var data = getDataFromSQL();
+    //console.log(data);
+    // res.render("login",{name: req.params.name, data : data});
+});
+
+
+
+
+
+function getDataFromSQL() {
+    var Connection = require('tedious').Connection;
+    var config = {
+        userName: 'MSM_Promotion_Admin',
+        password: 'Seebaer181050',
+        server: 'msm.database.windows.net',
+        // If you are on Microsoft Azure, you need this:
+        options: {encrypt: true, database: 'MSM_Promotion_Vorversion'}
+    };
+    var connection = new Connection(config);
+
+    connection.on('connect', function(err) {
+            // If no error, then good to go...
+            console.log("connected");
+            //    executeStatement();
+            queryDatabase();
+            //   queryDatabase2();
+        }
+    );
+
+    function executeStatement() {
+
+        var request = new Request("select * form dbo.LeistungskundeAccount",function(err,count,rows){
+        });
+        console.log("execunting statement")
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+                console.log("%s\t%s", column.metadata.colName, column.value);
+            });     console.log(column.value);
+        });
+        console.log("execunting statement")
+    }
+
+    function queryDatabase()
+    { console.log('Reading rows from the Table...');
+
+        // Read all rows from table
+        request = new Request(
+            "SELECT * from dbo.LeistungskundeAccount",
+            function(err, rowCount, rows)
+            {
+                console.log(rowCount + ' row(s) returned');
+                // process.exit();
+            }
+        );
+
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+                console.log("%s\t%s", column.metadata.colName, column.value);
+            });
+        });
+        connection.execSql(request);
+    }
+
+    function queryDatabase2() {
+        var result = [];
+        request = new Request("select * form dbo.LeistungskundeAccount",function(err,count,rows){
+            console.log(result);
+
+        });
+        request.on("row", function (columns) {
+            var item = {};
+            columns.forEach(function (column) {
+                item[column.metadata.colName] = column.value;
+                console.log(column.value);
+            });
+            result.push(item);
+        });
+        connection.execSql(request);
+        return result;
+    }
+}
+var port = process.env.PORT || 1337;
+app.listen(port);
