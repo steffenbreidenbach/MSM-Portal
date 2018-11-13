a3bf508d753c4e274cc0446c7885f31087032032
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-13 11:40:15 +0100

    4.2

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var http = require('http');
-var Request = require('tedious').Request;
-
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
