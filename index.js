232f3bd4ef00d8d3efc6f5c32c1cb7727c395a79
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-20 13:14:56 +0100

    update

--- index.js
+++ index.js
@@ -42,6 +42,10 @@ app.get("/home",function(req,res){
     });
 });
 
+app.get('*', function(req, res) {
+    res.sendFile(__dirname+"/public/html/404.html");
+});
+
 function createConnection(cb){
     var Connection = require('tedious').Connection;
     var Request = require('tedious').Request;
