7b91fbb743d92e0b400bb214d6d0b7999cd2b021
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-11-20 12:37:29 +0100

    4.2

--- public/js/LKundenAccount.js
+++ public/js/LKundenAccount.js
@@ -1,45 +1,10 @@
 
-var express = require("express");
-var app = express();
-var Request = require('tedious').Request;
 
-
-app.set("view engine","ejs");
-
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/public/html/login.html");
-});
-
-app.post("/index.html",function(req,res){
-    res.sendFile(__dirname + "/public/html/index.html");
-});
-
-app.get("/login",function(req,res){
-    res.sendFile(__dirname + "/public/html/login.html");
-});
-
-app.get("/steffen",function(req,res){
-    res.sendFile(__dirname+"/public/html/test.html");
-});
-
-app.get("/login/:name",function(req,res){
-    var data = ["jonathan","as","hhdahds","hdasd"];
-    data.push("test");
-    getDataFromSQL(data);
-     res.render("login",{name: req.params.name, data : data});
-});
-
-app.get("/getData/:id",function(req,res){
-    var data = ["test"];
-    var test = getDataFromSQL();
+var test = {};
+createConnection((data)=>{
+    test = data;
     console.log(test);
-    res.render("data",{id:req.params.id,data:test})
-});
-
-app.get("/home",function(req,res){
-    createConnection((data)=>{
-        res.render("index",{daten:data})
-    });
+    return test;
 });
 
 function createConnection(cb){
@@ -75,7 +40,3 @@ function createConnection(cb){
         connection.execSql(request);
     });
 }
-
-
-var port = process.env.PORT || 1337;
-app.listen(port);
