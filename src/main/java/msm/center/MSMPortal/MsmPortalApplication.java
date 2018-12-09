f7e7fcae4d89177736495b22ef72ffd833270eb6
Jonathan Peppler <jonathan-peppler@gmx.de>
2018-12-09 10:59:51 +0100

    v0.0.1

---
+++ src/main/java/msm/center/MSMPortal/MsmPortalApplication.java
@@ -0,0 +1,12 @@
+package msm.center.MSMPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsmPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsmPortalApplication.class, args);
+	}
+}
